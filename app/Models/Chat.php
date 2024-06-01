ae356e4b22054ec138a6e94ae4173a83336cc94c
Gon1203 <jygon931203@gmail.com>
2024-06-01 10:58:43 +0700

    feat: 사진 전송     * 미디어 라이브러리     * 사진 전송 / 수신     * 썸네일 처리

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class Chat extends Model implements HasMedia
 {
@@ -17,7 +18,10 @@ class Chat extends Model implements HasMedia
     protected $table = 'chats';
     protected $guarded = [];
 
-    protected $with = ['user'];
+    protected $with = [
+        'user',
+        'media'
+    ];
 
     public function chatRoom(): BelongsTo
     {
@@ -32,6 +36,14 @@ class Chat extends Model implements HasMedia
     public function registerMediaCollections(): void
     {
         $this->addMediaCollection('images');
+    }
+
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this->addMediaConversion('thumb')
+            ->width(368)
+            ->height(232)
+            ->sharpen(10);
     }
 
     protected function serializeDate(DateTimeInterface $date): string
--- resources/js/Pages/ChatRoom/Show.vue
+++ resources/js/Pages/ChatRoom/Show.vue
@@ -16,6 +16,13 @@
                             <div class="break-words">
                                 {{ item?.message }}
                             </div>
+                            <template v-if="item?.media?.length > 0">
+                                <div class="w-full h-full flex">
+                                    <div v-for="image in item.media" class="max-w-[30%]">
+                                        <img :src="image.original_url" alt="">
+                                    </div>
+                                </div>
+                            </template>
                         </div>
                     </div>
                 </template>
@@ -33,58 +40,87 @@
                             <div class="break-words">
                                 {{ item?.message }}
                             </div>
+                            <template v-if="item?.media?.length > 0">
+                                <div class="w-full h-full flex">
+                                    <div v-for="image in item.media" class="">
+                                        <img :src="image.original_url" alt="">
+                                    </div>
+                                </div>
+                            </template>
                         </div>
                     </div>
                 </template>
             </template>
         </div>
-        <input class="w-full" type="text" v-model="messageForm.message">
-        <button class="rounded-lg bg-gray-900 text-white border-2 px-4 py-2" @click="onClickSubmit"> SUBMIT </button>
+        <form class="flex" @submit.prevent="onClickSubmit">
+            <div class="relative">
+                <input v-model="messageForm.message" class="w-full" type="text">
+                <span
+                    class="absolute right-1.5 top-1.5 hover:cursor-pointer hover:font-semibold hover:border-gray-900 text-xl border-2 rounded-full px-2"
+                    @click="onclickAddMedia"
+                >
+                    +
+                </span>
+            </div>
+            <input id="media-input" accept="image/*" class="hidden" multiple type="file" @change="onChangeMediaInput">
+            <button class="rounded-lg bg-gray-900 text-white border-2 px-4 py-2" @click="onClickSubmit"> SUBMIT</button>
+        </form>
     </div>
 </template>
 
 <script setup>
-import { useForm, usePage } from "@inertiajs/vue3";
+import { useForm } from "@inertiajs/vue3";
 import { inject, ref } from "vue";
 
-const props = defineProps({
+const props = defineProps( {
     data: Object
-})
+} )
 
-const route = inject('route')
+const route = inject( 'route' )
 
-const messageForm = useForm({
+const messageForm = useForm( {
     messageType: 'T',
     message: null,
     images: FileList
-})
-function onClickSubmit(){
-    messageForm.post(route('chat.room.message.send',props?.data?.id),{
-        onSuccess:()=>{
-            console.log('success')
+} )
+
+function onClickSubmit() {
+    messageForm.post( route( 'chat.room.message.send', props?.data?.id ), {
+        onSuccess: () => {
+            console.log( 'success' )
             messageForm.reset()
         },
-        onError:(err)=>{
-            console.log(err)
+        onError: ( err ) => {
+            console.log( err )
         },
-        preserveState:true,
-        preserveScroll:true
-    });
+        preserveState: true,
+        preserveScroll: true
+    } );
 }
 
-const chatList = ref(props?.data?.chats ?? []);
+const chatList = ref( props?.data?.chats ?? [] );
 
 // Get the scrollable div element
 
 // Function to scroll to the bottom of the div
 function scrollToBottom() {
-    let scrollableDiv = document.getElementById('chatContainer');
+    let scrollableDiv = document.getElementById( 'chatContainer' );
     scrollableDiv.scrollTop = scrollableDiv.scrollHeight;
 }
-Echo.channel(`chatRoom.${props.data.id}`).listen('.message.sent',(e)=>{
-    chatList.value.push(e.chat);
+
+Echo.channel( `chatRoom.${ props.data.id }` ).listen( '.message.sent', ( e ) => {
+    chatList.value.push( e.chat );
     scrollToBottom()
-})
+} )
+
+function onclickAddMedia() {
+    document.getElementById( 'media-input' ).click()
+}
+
+function onChangeMediaInput( e ) {
+    messageForm.images = e.target.files
+}
+
 
 </script>
 
